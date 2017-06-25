8191479b1f9018275949f206bd329efb5996e396
PhilWolmarans <phil.wolmarans@gmail.com>
2017-06-25 16:45:54 +0200

    UpdateSite

---
+++ Products_Services/JavaScript/ProdServ.js
@@ -0,0 +1,42 @@
+var output = document.getElementById('output');
+
+var blank = "";
+var firstName = "Phillip";
+
+output.innerHTML-firstName;
+
+
+
+
+
+
+
+
+
+
+/*
+var a = 7;
+var b = "Character";
+var c = "this is a sample of a string";
+var myCar = "Isuzu bakkie";
+var myColor = "Metalic Brownish";
+var blank = "";
+
+var d = true;
+
+if (!d) {
+    //code
+    console.log('d is false');
+}
+else {
+    console.log('d is true');
+}
+
+                
+document.write("The car that you drive is a: "+ myColor+' <br>'+myCar);
+document.write('<br>');
+document.write(1+1+a);
+document.write('<br>');
+document.write(b);
+document.write('<br>');
+document.write(c); */
